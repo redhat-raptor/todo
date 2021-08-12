1c1eb512951490f0711334338bee6f39e8b2b868
Ahsanul <ahsanul.chowdhury@rci.rogers.com>
2021-08-12 13:36:32 -0400

    wip

--- app.py
+++ app.py
@@ -4,14 +4,16 @@ from dotenv import load_dotenv
 import redis
 
 load_dotenv()
-redis_client = redis.Redis(host=env.get('REDIS_HOST'), port=env.get('REDIS_PORT'), password=env.get('REDIS_PASSWORD'))
-redis_client.set('todo', '{"foo": "bar"}')
+redis_client = None
 
 app = Chalice(app_name='todo')
 
 
 @app.route('/todo')
 def index():
+    redis_connect()
+    redis_client.set('todo', '{"foo": "bar"}')
+    
     todo = redis_client.get('todo')
     return todo
 
@@ -21,3 +23,5 @@ def create_user():
     user_as_json = app.current_request.json_body
     return {'user': user_as_json}
 
+def redis_connect():
+    redis_client = redis.Redis(host=env.get('REDIS_HOST'), port=env.get('REDIS_PORT'), password=env.get('REDIS_PASSWORD'))

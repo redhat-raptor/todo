5752b0b892cafc97f805c4bbacb46b9d1a7fe95b
Ahsanul <ahsanul.chowdhury@rci.rogers.com>
2021-08-18 15:19:10 -0400

    fixes connect code

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from os import environ as env
 from dotenv import load_dotenv
 from os.path import join, dirname
 import redis
+import json
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
@@ -14,18 +15,17 @@ app = Chalice(app_name='todo')
 @app.route('/todo')
 def index():
     redis_connect()
-    redis_client.set('todo', '{"foo": "bar"}')
     todo = redis_client.get('todo')
     return todo
 
 
 @app.route('/todo', methods=['POST'])
 def create_user():
-    user_as_json = app.current_request.json_body
-    return {'user': user_as_json}
+    redis_connect()
+    todo_payload = app.current_request.json_body
+    redis_client.set('todo', json.dumps(todo_payload))
+    return todo_payload
+
 
 def redis_connect():
     redis_client = redis.Redis(host=env.get('REDIS_HOST'), port=env.get('REDIS_PORT'), password=env.get('REDIS_PASSWORD'))
-
-
-redis_connect()

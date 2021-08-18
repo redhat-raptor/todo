a9b20a76729b19f8673affaee74bdaafede443b9
Ahsanul <ahsanul.chowdhury@rci.rogers.com>
2021-08-18 15:40:00 -0400

    fixes connection

--- app.py
+++ app.py
@@ -1,31 +1,35 @@
-from chalice import Chalice
+from chalice import Chalice, Response
 from os import environ as env
-from dotenv import load_dotenv
-from os.path import join, dirname
 import redis
 import json
 
-dotenv_path = join(dirname(__file__), '.env')
-load_dotenv(dotenv_path)
-redis_client = None
-
 app = Chalice(app_name='todo')
-
 
 @app.route('/todo')
 def index():
-    redis_connect()
-    todo = redis_client.get('todo')
-    return todo
-
+    try:
+        redis_client = redis_connect()
+    except Exception as e:
+        return Response(body='Error connecting backend!',
+                    headers={'Content-Type': 'text/plain'},
+                    status_code=500)
+    
+    try:
+        todo_content = redis_client.get('todo')
+    except Exception as e:
+        return Response(body='Error getting data from backend!',
+                    headers={'Content-Type': 'text/plain'},
+                    status_code=500)
+    
+    return json.loads(todo_content)
 
 @app.route('/todo', methods=['POST'])
 def create_user():
-    redis_connect()
+    redis_client = redis_connect()
     todo_payload = app.current_request.json_body
     redis_client.set('todo', json.dumps(todo_payload))
     return todo_payload
 
 
 def redis_connect():
-    redis_client = redis.Redis(host=env.get('REDIS_HOST'), port=env.get('REDIS_PORT'), password=env.get('REDIS_PASSWORD'))
+    return redis.Redis(host=env.get('REDIS_HOST'), port=env.get('REDIS_PORT'), password=env.get('REDIS_PASSWORD'))
